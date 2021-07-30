7a0ad5d485bfd5c9040ff2dbca7406ba8e000716
Vamsi Pachamatla <vamsiraju835@gmail.com>
2021-07-30 12:30:20 +0530

    Initial Commit

---
+++ 8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  8Ball
+//
+//  Created by vamsi pachamatla on 28/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var eightBallImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        eightBallImage.image = #imageLiteral(resourceName: "ball4")
+        
+    }
+
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        eightBallImage.image = [#imageLiteral(resourceName: "ball1") , #imageLiteral(resourceName: "ball2") , #imageLiteral(resourceName: "ball5") , #imageLiteral(resourceName: "ball4") , #imageLiteral(resourceName: "ball3")][Int.random(in: 0...4)]
+    }
+    
+}
+
